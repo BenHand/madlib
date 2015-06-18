9018f36979b2faa9926a5c9932a5c43b42fa6d1f
Benjamin Hand <gneeshot@gmail.com>
2015-06-18 15:09:14 -0500

    updating with the changes made since cloning

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,8 @@
 //= require backbone
 //= require react
 //= require moment
+//= require_directory ./models
+//= require_directory ./collections
 //= require_directory ./components
 //= require_tree .
 
