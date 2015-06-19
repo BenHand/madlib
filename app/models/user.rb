932a44591cb0747cb0047833297cf2fabbb66f50
Benjamin Hand <gneeshot@gmail.com>
2015-06-19 11:22:35 -0500

    removing incorrect validation of username, changed to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates_presence_of :username, :email,
+  validates_presence_of :name, :email,
                         :password,
                         :password_confirmation
   has_secure_password
