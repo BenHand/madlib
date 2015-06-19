5837163ea6498bbe69bce2ddeda610cc25facace
Benjamin Hand <gneeshot@gmail.com>
2015-06-19 11:07:42 -0500

    added in methods within users/quotes/madquotes controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@ OriginalQuote.create(author: 'Albert Einstein',
                       quote: 'Learn from yesterday, live for today, hope for tomorrow.
                                The important thing is not to stop questioning.',
                 input_quote: "['Learn from ', ' live for ', ' hope for ', '. The important thing is not to stop ', '.' ]")
+OriginalQuote.create(author: 'Dr. Seuss',
+                      quote: 'Only you can control your future.',
+                input_quote: "['Only ', ' can control your ', '.' ]")
+OriginalQuote.create(author: 'Jessie Pinkman',
+                      quote: "Yeah bitch! Magnets!",
+                input_quote: "['Yeah bitch! ', '!']")
