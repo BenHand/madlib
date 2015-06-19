810418d9f9e1a7279e27b77d595affd9099ef589
Benjamin Hand <gneeshot@gmail.com>
2015-06-19 09:13:12 -0500

    added in sessions, put a show and index method for quotes. added a quote to seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-  def index
+
+  before_filter :current_user
+
+  def current_user
+    if @current_user.nil?
+      if session[:user_id].present?
+        @current_user = User.find(session[:user_id])
+      end
+    else
+      @current_user
+    end
   end
+
+  helper_method :current_user
+
+  def authenticate_user!
+    if current_user == nil
+      redirect_to login_path, alert: 'You must be logged in.'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                 :password_confirmation, :bio, :pic_url)
+  end
+
 end
