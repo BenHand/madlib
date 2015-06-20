8f4aeabe53f5fb5cba4db5082c2db109fa96ba1c
Benjamin Hand <gneeshot@gmail.com>
2015-06-19 20:53:26 -0500

    added a bunch of quotes to seed file TOBE dissected

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,110 @@
-OriginalQuote.create(
-  author: 'Albert Einstein',
+OriginalQuote.create(author: 'Albert Einstein',
                       quote: 'Learn from yesterday, live for today, hope for tomorrow.
                                The important thing is not to stop questioning.',
-                input_quote: 'Learn from __NOUN__ live for today, hope for __VERB__. The important thing is not to stop __ADJECTIVE__.')
-OriginalQuote.create(author: 'Dr. Seuss',
-                      quote: 'Only you can control your future.',
-                input_quote: 'Only __NOUN__ can control your __ADJECTIVE__.')
-OriginalQuote.create(author: 'Jessie Pinkman',
-                      quote: "Yeah bitch! Magnets!",
-                input_quote: 'Yeah bitch! __NOUN__!')
-OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
-# OriginalQuote.create(author: "", quote: "", input_quote: "")
+                input_quote: 'Learn from __NOUN__, live for today, hope for __VERB__. The important thing is not to stop __ADJECTIVE__.')
+OriginalQuote.create(author: "Jaden Smith",
+                      quote: "School Is The Tool To Brainwash The Youth.",
+                input_quote: "School Is The Tool To Brainwash The Youth.")
+OriginalQuote.create(author: "Oscar Gamble",
+                      quote: "They don't think it be like it is, but it do",
+                input_quote: "They don't think it be like it is, but it do")
+OriginalQuote.create(author: "Michelle Bachmann",
+                      quote: "President Obama can't wait to get Americans addicted to the crack cocaine of dependency.",
+                input_quote: "President Obama can't wait to get Americans addicted to the crack cocaine of dependency.")
+OriginalQuote.create(author: "Captain Nemo",
+                      quote: "I shall call him Squishy. And he shall be mine. And he shall be MY Squishy.",
+                input_quote: "I shall call him Squishy. And he shall be mine. And he shall be MY Squishy.")
+OriginalQuote.create(author: "Han Solo",
+                      quote: "Hokey religions and ancient weapons are no match for a good blaster by your side, kid.",
+                input_quote: "Hokey religions and ancient weapons are no match for a good blaster by your side, kid.")
+OriginalQuote.create(author: "Jimmie Dimmick",
+                      quote: "Now, you've got a corpse in a car, minus a head, in a garage. Take me to it.",
+                input_quote: "Now, you've got a corpse in a car, minus a head, in a garage. Take me to it.")
+OriginalQuote.create(author: "Bull Durham",
+                      quote: "Sometimes you win, sometimes you lose, and sometimes it rains.",
+                input_quote: "Sometimes you win, sometimes you lose, and sometimes it rains.")
+OriginalQuote.create(author: "Ron Burgandy",
+                      quote: "They've done studies, you know. Sixty percent of the time, it works every time.",
+                input_quote: "They've done studies, you know. Sixty percent of the time, it works every time.")
+OriginalQuote.create(author: "Andy Dufresne",
+                      quote: "Hope is a good thing, maybe the best of things, and no good thing ever dies.",
+                input_quote: "Hope is a good thing, maybe the best of things, and no good thing ever dies.")
+OriginalQuote.create(author: "Peaches Monroe",
+                      quote: "We in dis bitch, finna get crunk, eyebrows on fleek, dafuq.",
+                input_quote: "We in dis bitch, finna get crunk, eyebrows on fleek, dafuq.")
+OriginalQuote.create(author: "Bob Marley",
+                      quote: "Truth is, everybody is going to hurt you; you just gotta find the ones worth suffering for.",
+                input_quote: "Truth is, everybody is going to hurt you; you just gotta find the ones worth suffering for.")
+OriginalQuote.create(author: "Bill Gates",
+                      quote: "We all need people who will give us feedback. That's how we improve.",
+                input_quote: "We all need people who will give us feedback. That's how we improve.")
+OriginalQuote.create(author: "Abraham Lincoln",
+                      quote: "Don’t believe everything you read on the internet.",
+                input_quote: "Don’t believe everything you read on the internet.")
+OriginalQuote.create(author: "Joseph Nash",
+                      quote: "There is no shred of evidence in favor of the idea that life is serious.",
+                input_quote: "There is no shred of evidence in favor of the idea that life is serious.")
+OriginalQuote.create(author: "Isaac Asimov",
+                      quote: "Life is pleasant. Death is peaceful. It's the transition that's troublesome.",
+                input_quote: "Life is pleasant. Death is peaceful. It's the transition that's troublesome.")
+OriginalQuote.create(author: "Isaac Asimov",
+                      quote: "If knowledge can create problems, it is not through ignorance that we can solve them.",
+                input_quote: "If knowledge can create problems, it is not through ignorance that we can solve them.")
+OriginalQuote.create(author: "Barack Obama",
+                      quote: "If you're walking down the right path and you're willing to keep walking, eventually you'll make progress.",
+                input_quote: "If you're walking down the right path and you're willing to keep walking, eventually you'll make progress.")
+OriginalQuote.create(author: "Barack Obama",
+                      quote: "That's the good thing about being president, I can do whatever I want.",
+                input_quote: "That's the good thing about being president, I can do whatever I want.")
+OriginalQuote.create(author: "Michael Jackson",
+                      quote: "I'm just like anyone. I cut and I bleed. And I embarass easily.",
+                input_quote: "I'm just like anyone. I cut and I bleed. And I embarass easily.")
+OriginalQuote.create(author: "Sun Tzu",
+                      quote: "If you know the enemy and know yourself you need not fear the results of a hundred battles.",
+                input_quote: "If you know the enemy and know yourself you need not fear the results of a hundred battles.")
+OriginalQuote.create(author: "Sun Tzu",
+                      quote: "The supreme art of war is to subdue the enemy without fighting.",
+                input_quote: "The supreme art of war is to subdue the enemy without fighting.")
+OriginalQuote.create(author: "E.A. Poe",
+                      quote: "Once upon a midnight dreary, while I pondered weak and weary.",
+                input_quote: "Once upon a midnight dreary, while I pondered weak and weary.")
+OriginalQuote.create(author: "Forrest Gump",
+                      quote: "My momma always said, 'Life was like a box of chocolates. You never know what you're gonna get.'",
+                input_quote: "My momma always said, 'Life was like a box of chocolates. You never know what you're gonna get.'")
+OriginalQuote.create(author: "Ferris Bueller",
+                      quote: "Life moves pretty fast. You don't stop and look around once in a while, you could miss it.",
+                input_quote: "Life moves pretty fast. You don't stop and look around once in a while, you could miss it.")
+OriginalQuote.create(author: "The Batman",
+                      quote: "I have given a name to my pain, and it is Batman.",
+                input_quote: "I have given a name to my pain, and it is Batman.")
+OriginalQuote.create(author: "Lester Burnham",
+                      quote: "Remember those posters that said, 'Today is the first day of the rest of your life'? Well, that's true of every day but one...the day you die.",
+                input_quote: "Remember those posters that said, 'Today is the first day of the rest of your life'? Well, that's true of every day but one...the day you die.")
+OriginalQuote.create(author: "Gluteus Maximus",
+                      quote: "Are you not entertained?! Are you not entertained?! Is this not why you are here?",
+                input_quote: "Are you not entertained?! Are you not entertained?! Is this not why you are here?")
+OriginalQuote.create(author: "Princess Leia",
+                      quote: "Help me, Obi-Wan Kenobi, you're my only hope.",
+                input_quote: "Help me, Obi-Wan Kenobi, you're my only hope.")
+OriginalQuote.create(author: "Truman Burbank",
+                      quote: "Good morning, and in case I don't see you, good afternoon, good evening, and good night!",
+                input_quote: "Good morning, and in case I don't see you, good afternoon, good evening, and good night!")
+# OriginalQuote.create(author: ,
+#                       quote: ,
+#                 input_quote: )
+# OriginalQuote.create(author: ,
+#                       quote: ,
+#                 input_quote: )
+# OriginalQuote.create(author: ,
+#                       quote: ,
+#                 input_quote: )
+# OriginalQuote.create(author: ,
+#                       quote: ,
+#                 input_quote: )
+# OriginalQuote.create(author: ,
+#                       quote: ,
+#                 input_quote: )
 Inspiration.create(inspire: "The measure of who we are is what we do with what we have.")
 Inspiration.create(inspire: "If you can dream it, you can do it.")
+
+
