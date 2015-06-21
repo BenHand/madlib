927bffd18a9c8b1dc1cae748873bb832c5e730b5
Joshua Lagrange <jlagrange87@gmail.com>
2015-06-21 12:52:23 -0500

    put in validation on front end

--- app/assets/javascripts/models/UserModel.js.jsx
+++ app/assets/javascripts/models/UserModel.js.jsx
@@ -9,6 +9,29 @@ var UserModel = Backbone.Model.extend({
 	pic_url:null,
 	quote_count:null
 	},
+
 	urlRoot:"users",
-	idAttribute:"id"
+	idAttribute:"id",
+
+	validate: function(attr) {
+		if(!attr.name) {
+			return "Please enter a valid name.";
+		}
+		else if(!attr.email){
+			return 'You must enter an email.'
+		}
+		else if(attr.email.indexOf("@") == -1){
+			return 'Email must be in the correct format.'
+		}
+		else if(!attr.password) {
+			return 'You must enter a password.';
+		}
+		else if(attr.password != attr.password_confirmation){
+			return 'You must enter the same password.';
+		}
+		else if(attr.password.length < 6) {
+			return 'Your password must be at least six characters.';
+		}
+		return false;
+	}
 })
