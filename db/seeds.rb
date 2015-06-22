d174f5c1d7bb8e179ba5ee65871cc9832c0faff1
Joshua Lagrange <jlagrange87@gmail.com>
2015-06-21 22:19:02 -0500

    changed some quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,97 +7,97 @@ OriginalQuote.create(author: "Jaden Smith",
                 input_quote: "__NOUN__ Is The Tool To __VERB__ The __NOUN__.")
 OriginalQuote.create(author: "Oscar Gamble",
                       quote: "They don't think it be like it is, but it do",
-                input_quote: "__NOUN__ don't __VERB__ it be like it is, but it __VERB__")
+                input_quote: "__NOUN__ don't __VERB__ it be like it is, but it __ADJECTIVE__")
 OriginalQuote.create(author: "Michelle Bachmann",
                       quote: "President Obama can't wait to get Americans addicted to the crack cocaine of dependency.",
                 input_quote: "President __NOUN__ can't __VERB__ to get Americans addicted to the __ADJECTIVE__ cocaine of dependency.")
 OriginalQuote.create(author: "Captain Nemo",
                       quote: "I shall call him Squishy. And he shall be mine. And he shall be MY Squishy.",
-                input_quote: "I shall __VERB__ him Squishy. And __NOUN__ shall be mine. And he shall be MY __NOUN__.")
+                input_quote: "I shall __VERB__ him __NOUN__. And he shall be mine. And he shall be __ADJECTIVE__ Squishy.")
 OriginalQuote.create(author: "Han Solo",
                       quote: "Hokey religions and ancient weapons are no match for a good blaster by your side, kid.",
-                input_quote: "Hokey __NOUN__ and __NOUN__ weapons are no match for a __ADJECTIVE__ blaster by your side, kid.")
+                input_quote: "Hokey __NOUN__ and __ADJECTIVE__ weapons are no __VERB__ for a good blaster by your side, kid.")
 OriginalQuote.create(author: "Jimmie Dimmick",
                       quote: "Now, you've got a corpse in a car, minus a head, in a garage. Take me to it.",
                 input_quote: "Now, you've got a __NOUN__ in a car, minus a __NOUN__, in a __NOUN__. Take me to it.")
 OriginalQuote.create(author: "Bull Durham",
                       quote: "Sometimes you win, sometimes you lose, and sometimes it rains.",
-                input_quote: "Sometimes __NOUN__ win, sometimes you __VERB__, and sometimes __NOUN__ rains.")
+                input_quote: "Sometimes __NOUN__ win, sometimes you __VERB__, and sometimes it rains.")
 OriginalQuote.create(author: "Ron Burgandy",
                       quote: "They've done studies, you know. Sixty percent of the time, it works every time.",
-                input_quote: "They've done __NOUN__, you know. __ADJECTIVE__ percent of the time, it works every time.")
+                input_quote: "They've done __NOUN__, you know. __ADJECTIVE__ percent of the time, it __VERB__s every time.")
 OriginalQuote.create(author: "Andy Dufresne",
                       quote: "Hope is a good thing, maybe the best of things, and no good thing ever dies.",
-                input_quote: "__NOUN__ is a good thing, maybe the __NOUN__ of things, and no __ADJECTIVE__ thing ever dies.")
+                input_quote: "__NOUN__ is a good thing, maybe the best of things, and no __ADJECTIVE__ thing ever __VERB__s.")
 OriginalQuote.create(author: "Peaches Monroe",
                       quote: "We in dis bitch, finna get crunk, eyebrows on fleek, dafuq.",
-                input_quote: "We in dis __NOUN__, finna get crunk, __NOUN__ on fleek, __ADJECTIVE__.")
+                input_quote: "We in dis __NOUN__, finna __VERB__ crunk, eyebrows on __ADJECTIVE__, dafuq.")
 OriginalQuote.create(author: "Bob Marley",
                       quote: "Truth is, everybody is going to hurt you; you just gotta find the ones worth suffering for.",
-                input_quote: "Truth is, __NOUN__ is going to __VERB__ you; you just gotta find the __NOUN__ worth suffering for.")
+                input_quote: "Truth is, __NOUN__ is going to __VERB__ you; you just gotta find the __ADJECTIVE__ worth suffering for.")
 OriginalQuote.create(author: "Bill Gates",
                       quote: "We all need people who will give us feedback. That's how we improve.",
-                input_quote: "We all need __NOUN__ who will give us feedback. That's __ADJECTIVE__ we improve.")
+                input_quote: "We all need __NOUN__ who will __VERB__ us feedback. That's __ADJECTIVE__ we improve.")
 OriginalQuote.create(author: "Abraham Lincoln",
                       quote: "Don’t believe everything you read on the internet.",
                 input_quote: "Don’t believe everything you __VERB__ on the __NOUN__.")
 OriginalQuote.create(author: "Joseph Nash",
                       quote: "There is no shred of evidence in favor of the idea that life is serious.",
-                input_quote: "There is no shred of __NOUN__ in favor of the idea that life is __ADJECTIVE__.")
+                input_quote: "There is no shred of __NOUN__ in __VERB__ of the idea that life is __ADJECTIVE__.")
 OriginalQuote.create(author: "Isaac Asimov",
                       quote: "Life is pleasant. Death is peaceful. It's the transition that's troublesome.",
                 input_quote: "Life is pleasant. Death is __ADJECTIVE__. It's the __NOUN__ that's troublesome.")
 OriginalQuote.create(author: "Isaac Asimov",
                       quote: "If knowledge can create problems, it is not through ignorance that we can solve them.",
-                input_quote: "If __NOUN__ can create problems, it is not through ignorance that we can __VERB__ them.")
+                input_quote: "If __NOUN__ can create problems, it is not through __ADJECTIVE__ that we can __VERB__ them.")
 OriginalQuote.create(author: "Barack Obama",
                       quote: "If you're walking down the right path and you're willing to keep walking, eventually you'll make progress.",
-                input_quote: "If you're __VERB__ down the right path and you're __ADJECTIVE__ to keep walking, eventually you'll make __NOUN__.")
+                input_quote: "If __NOUN__ 're walking down the right path and you're __ADJECTIVE__ to keep walking, eventually you'll __VERB__ progress.")
 OriginalQuote.create(author: "Barack Obama",
                       quote: "That's the good thing about being president, I can do whatever I want.",
-                input_quote: "That's the __ADJECTIVE__ thing about being __NOUN__, I can do whatever I want.")
+                input_quote: "That's the __ADJECTIVE__ thing about being __NOUN__, I can __VERB__ whatever I want.")
 OriginalQuote.create(author: "Sun Tzu",
                       quote: "If you know the enemy and know yourself you need not fear the results of a hundred battles.",
-                input_quote: "If you know the __NOUN__ and know yourself you need not fear the results of a hundred battles.")
+                input_quote: "If you know the __NOUN__ and know yourself __NOUN__ need not fear the results of a __ADJECTIVE__ battles.")
 OriginalQuote.create(author: "Sun Tzu",
                       quote: "The supreme art of war is to subdue the enemy without fighting.",
-                input_quote: "The __ADJECTIVE__ art of war is to subdue the enemy without fighting.")
+                input_quote: "The __ADJECTIVE__ art of war is to __VERB__ the __NOUN__ without fighting.")
 OriginalQuote.create(author: "E.A. Poe",
                       quote: "Once upon a midnight dreary, while I pondered weak and weary.",
-                input_quote: "Once upon a __NOUN__ dreary, while I pondered __ADJECTIVE__ and weary.")
+                input_quote: "Once upon a midnight dreary, while __NOUN__ pondered __ADJECTIVE__ and weary.")
 OriginalQuote.create(author: "Forrest Gump",
                       quote: "My momma always said, 'Life was like a box of chocolates. You never know what you're gonna get.'",
-                input_quote: "My __NOUN__ always said, 'Life was like a box of chocolates. You __ADJECTIVE__ know what you're gonna get.'")
+                input_quote: "My __NOUN__ always said, 'Life was like a box of chocolates. You __ADJECTIVE__ know what you're gonna __VERB__.'")
 OriginalQuote.create(author: "Ferris Bueller",
                       quote: "Life moves pretty fast. You don't stop and look around once in a while, you could miss it.",
                 input_quote: "__NOUN__ moves pretty __ADJECTIVE__. You don't stop and __VERB__ around once in a while, you could miss it.")
 OriginalQuote.create(author: "The Batman",
                       quote: "I have given a name to my pain, and it is Batman.",
-                input_quote: "I have given a name to my pain, and it is __NOUN__.")
+                input_quote: "I have given a name to my __ADJECTIVE__, and it is __NOUN__.")
 OriginalQuote.create(author: "Lester Burnham",
                       quote: "Remember those posters that said, 'Today is the first day of the rest of your life'? Well, that's true of every day but one...the day you die.",
                 input_quote: "__VERB__ those posters that said, 'Today is the first __NOUN__ of the rest of your life'? Well, that's __ADJECTIVE__ of every day but one...the day you die.")
 OriginalQuote.create(author: "Gluteus Maximus",
                       quote: "Are you not entertained?! Are you not entertained?! Is this not why you are here?",
-                input_quote: "Are you not __VERB__?! Are you not __VERB__?! Is this not why you are __NOUN__?")
+                input_quote: "Are you not __ADJECTIVE__?! Are you not entertained?! Is this not why you are __NOUN__?")
 OriginalQuote.create(author: "Princess Leia",
                       quote: "Help me, Obi-Wan Kenobi, you're my only hope.",
-                input_quote: "__NOUN__ me, Obi-Wan Kenobi, you're my only __VERB__.")
+                input_quote: "__VERB__ me, __NOUN__, you're my __ADJECTIVE__ hope.")
 OriginalQuote.create(author: "Truman Burbank",
                       quote: "Good morning, and in case I don't see you, good afternoon, good evening, and good night!",
                 input_quote: "__ADJECTIVE__ morning, and in case I don't __VERB__ you, good afternoon, good evening, and good __NOUN__ !")
 OriginalQuote.create(author: "Dorothy Gale",
                       quote: "Toto, i’ve got a feeling we’re not in kansas anymore.",
-                input_quote: "Toto, i’ve got a feeling we’re not in __NOUN__ anymore.")
+                input_quote: "Toto, i’ve got a feeling we’re __ADJECTIVE__ in __NOUN__ anymore.")
 OriginalQuote.create(author: "Martin Brody",
                       quote: "You’re gonna need a bigger boat.",
-                input_quote: "You’re gonna __VERB__ a bigger __NOUN__.")
+                input_quote: "You’re gonna __VERB__ a __ADJECTIVE__ __NOUN__.")
 OriginalQuote.create(author: "Cole Sear",
                       quote: "I see dead people.",
-                input_quote: "I see __ADJECTIVE__ people.")
+                input_quote: "I __VERB__ __ADJECTIVE__ __NOUN__.")
 OriginalQuote.create(author: "Wicked Witch",
                       quote: "I’ll get you, my pretty, and you’re little dog too!",
-                input_quote: "I’ll get you, my __ADJECTIVE__, and you’re little __NOUN__ too!")
+                input_quote: "I’ll __VERB__ you, my __ADJECTIVE__, and you’re little __NOUN__ too!")
 OriginalQuote.create(author: "Jack Dawson",
                       quote: "I’m the king of the world!",
                 input_quote: "I’m the __NOUN__ of the world!")
@@ -118,40 +118,40 @@ OriginalQuote.create(author: "N.DG. Tyson",
                 input_quote: "People don't think the __NOUN__ be __ADJECTIVE__ it is, but it do.")
 OriginalQuote.create(author: "Vince Vaughn",
                       quote: "You're so money and you don't even know it.",
-                input_quote: "You're so __NOUN__ and you don't even __VERB__ it.")
+                input_quote: "You're so __ADJECTIVE__ and you don't even __VERB__ it.")
 OriginalQuote.create(author: "Ron Burgundy",
                       quote: "I don't know how to put this, but I'm kind of a big deal. People know me. I'm very important. I have many leather-bound books and my apartment smells of rich mahogany.",
                 input_quote: "I don't know how to __VERB__ this, but I'm kind of a big deal. People know me. I'm very important. I have many leather-bound __NOUN__ and my apartment smells of __ADJECTIVE__ mahogany.")
 OriginalQuote.create(author: "Harry Callahan",
                       quote: "You’ve got to ask yourself one question: ‘Do I feel lucky?’ Well, do ya, punk?",
-                input_quote: "You’ve __VERB__ to ask yourself one __NOUN__: ‘Do I feel __ADJECTIVE__?’ Well, do ya, punk?")
+                input_quote: "You’ve got to __VERB__ yourself one __NOUN__: ‘Do I feel __ADJECTIVE__?’ Well, do ya, punk?")
 OriginalQuote.create(author: "George Taylor",
                       quote: "Get your stinking paws off me, you damned dirty ape!",
                 input_quote: "__VERB__ your __ADJECTIVE__ paws off me, you damned dirty __NOUN__!")
-# OriginalQuote.create(author: "Bill Kilgore",
-#                       quote: "I love the smell of napalm in the morning!",
-#                 input_quote: "I love the smell of napalm in the morning!")
-# OriginalQuote.create(author: "Thomas Edison",
-#                       quote: "Genius is one per cent inspiration, ninety-nine per cent perspiration.",
-#                 input_quote: "Genius is one per cent inspiration, ninety-nine per cent perspiration.")
-# OriginalQuote.create(author: "Mae West",
-#                       quote: "Is that a gun in your pocket, or are you just glad to see me?",
-#                 input_quote: "Is that a gun in your pocket, or are you just glad to see me?")
-# OriginalQuote.create(author: "Stanley Kubrick",
-#                       quote: "The great nations have always acted like gangsters, and the small nations like prostitutes.",
-#                 input_quote: "The great nations have always acted like gangsters, and the small nations like prostitutes.")
-# OriginalQuote.create(author: "Alfred Hitchcock",
-#                       quote: "Television has brought back murder into the home - where it belongs.",
-#                 input_quote: "Television has brought back murder into the home - where it belongs.")
-# OriginalQuote.create(author: "",
-#                       quote: "",
-#                 input_quote: "")
-# OriginalQuote.create(author: "",
-#                       quote: "",
-#                 input_quote: "")
-# OriginalQuote.create(author: "",
-#                       quote: "",
-#                 input_quote: "")
+OriginalQuote.create(author: "Bill Kilgore",
+                      quote: "I love the smell of napalm in the morning!",
+                input_quote: "I love the __VERB__ of __NOUN__ in the morning!")
+OriginalQuote.create(author: "Thomas Edison",
+                      quote: "Genius is one per cent inspiration, ninety-nine per cent perspiration.",
+                input_quote: "__NOUN__ is one per cent __ADJECTIVE__, ninety-nine per cent perspiration.")
+OriginalQuote.create(author: "Mae West",
+                      quote: "Is that a gun in your pocket, or are you just glad to see me?",
+                input_quote: "Is that a __NOUN__ in your pocket, or are you just __ADJECTIVE__ to __VERB__ me?")
+OriginalQuote.create(author: "Stanley Kubrick",
+                      quote: "The great nations have always acted like gangsters, and the small nations like prostitutes.",
+                input_quote: "The great __NOUN__s have always acted like gangsters, and the __ADJECTIVE__ nations like prostitutes.")
+OriginalQuote.create(author: "Alfred Hitchcock",
+                      quote: "Television has brought back murder into the home - where it belongs.",
+                input_quote: "__NOUN__ has brought back __VERB__ into the home - where it belongs.")
+OriginalQuote.create(author: "Theodore Roosevelt",
+                      quote: "Speak softly and carry a big stick; you will go far.",
+                input_quote: "__VERB__ softly and carry a __ADJECTIVE__ __NOUN__; you will go far.")
+OriginalQuote.create(author: "Lao Tzu",
+                      quote: "A journey of a thousand miles must begin with a single step.",
+                input_quote: "A journey of a thousand __NOUN__ must __VERB__ with a __ADJECTIVE__ step.")
+OriginalQuote.create(author: "Jack Byrnes",
+                      quote: "...I have nipples, Greg. Could you milk me?",
+                input_quote: "...I have __NOUN__, Greg. Could you __VERB__ me?")
 # OriginalQuote.create(author: "",
 #                       quote: "",
 #                 input_quote: "")
@@ -170,7 +170,7 @@ Inspiration.create(inspire: "Never think about the mistakes you've made. Think a
 Inspiration.create(inspire: "Every artist was first an amateur.")
 Inspiration.create(inspire: "Nothing is impossible, the word itself says 'I'm possible!'")
 
-User.create(name: 'ben', email: 'ben@ben.com',
+User.create(name: 'Ben', email: 'ben@ben.com',
         password: 'benspw', password_confirmation: 'benspw')
 User.create(name: 'Casie', email: 'casie@casie.com',
         password: 'casie', password_confirmation: 'casie')
